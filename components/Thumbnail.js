84d3116e49b73ab6ef3dfb55309a8a0053dd69f0
Rohit Ranjan <arohitranjan11@gmail.com>
2021-08-30 00:38:16 +0530

    few changes

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
-
+// eslint-disable-next-line react/display-name
 const Thumbnail = forwardRef(({ result }, ref) => {
      const BASE_URL = "https://image.tmdb.org/t/p/original";
     return (
